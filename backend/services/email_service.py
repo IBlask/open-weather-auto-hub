b22f8c7362dc043fd0547862e6cdd731312b8f7d
IBlask <95757169+IBlask@users.noreply.github.com>
2025-02-21 22:39:35 +0100

    Add email validator on new-email endpoint

--- backend/services/email_service.py
+++ backend/services/email_service.py
@@ -1,13 +1,21 @@
 from flask import jsonify
 from sqlalchemy.exc import IntegrityError
+import re
 
 from app import db
 from models import Email
+
+def is_valid_email(email_address):
+    regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.match(regex, email_address) is not None
 
 
 def add_email(email_address):
     if not email_address or email_address.isspace():
         return 'Email address is required!', 400
+    
+    if not is_valid_email(email_address):
+        return 'Invalid email address!', 400
     
     try:
         email = Email(email = email_address)
