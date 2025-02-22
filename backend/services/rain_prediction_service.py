870ef0734626c0a7662ccd57a26a1985830be187
IBlask <95757169+IBlask@users.noreply.github.com>
2025-02-23 00:15:00 +0100

    Add rain prediction model

---
+++ backend/services/rain_prediction_service.py
@@ -0,0 +1,99 @@
+from datetime import datetime, timedelta
+
+from app import db
+from models import Temperature, Humidity, Pressure, Wind, RainPrediction
+
+PRESSURE_ABS_SHARE = 0.2                # absoulte pressure value -> 1010 hPa 
+PRESSURE_DELTA_SHARE = 0.15             # pressure change over time: x -> 1010 hPa
+HUMIDITY_ABS_SHARE = 0.15               # absolute humidity value -> 100%
+HUMIDITY_DELTA_SHARE = 0.1              # humidity change over time: x -> 100%
+WIND_SPEED_SHARE = 0.05                 # wind speed -> 15 km/h
+WIND_DIRECTION_DELTA_SHARE = 0.2        # wind direction -> 220 degrees
+WIND_DIRECTION_CHANGE_SHARE = 0.05      # wind direction change over time: x -> 220 degrees
+TEMPERATURE_ABS_SHARE = 0.05            # temperature -> 5-20 degrees celsius
+TEMPERATURE_DELTA_SHARE = 0.05          # temperature change over time: x -> 5-20 degrees celsius
+
+def get_data():
+    timeframe_min = datetime.now() - timedelta(hours = 1)
+
+    temperature = Temperature.query.filter(Temperature.created_at >= timeframe_min).all()
+    humidity = Humidity.query.filter(Humidity.created_at >= timeframe_min).all()
+    pressure = Pressure.query.filter(Pressure.created_at >= timeframe_min).all()
+    wind = Wind.query.filter(Wind.created_at >= timeframe_min).all()
+
+    return {
+        'temperature': temperature,
+        'humidity': humidity,
+        'pressure': pressure,
+        'wind': wind
+    }
+
+
+def predict_rain():
+    data = get_data()
+
+    if not data['humidity'] or not data['pressure']:
+        raise ValueError('Insufficient data to predict rain!')
+
+    if data['humidity'][-1].value > 90 and data['pressure'][-1].value < 1010:
+        prediction = 1
+    else:
+        prediction = 0
+
+        pressure_delta = 1 - ((data['pressure'][-1].value - 1010) / 5)
+        pressure_change = data['pressure'][-1].value - data['pressure'][0].value
+        pressure_change_time = data['pressure'][-1].created_at - data['pressure'][0].created_at
+        pressure_change_delta = pressure_change / (pressure_change_time.total_seconds() / 60) * 60
+
+        humidity_delta = data['humidity'][-1].value - data['humidity'][0].value
+
+        wind_speed_mean = sum([wind.speed for wind in data['wind']]) / len(data['wind'])
+        wind_direction_norm_first = data['wind'][0].direction if data['wind'][0].direction - 40 >= 0 else 360 - (40 - data['wind'][0].direction)
+        wind_direction_norm_last = data['wind'][-1].direction if data['wind'][-1].direction - 40 >= 0 else 360 - (40 - data['wind'][-1].direction)
+        wind_direction_delta = abs(180 - wind_direction_norm_last)
+        wind_direction_change_delta = abs(180 - wind_direction_norm_first) - abs(180 - wind_direction_norm_last)
+
+        temperature_delta =  abs(13 - data['temperature'][-1].value)
+        temperature_change_delta = abs(13 - data['temperature'][0].value) - abs(13 - data['temperature'][-1].value)
+
+
+        if (data['pressure'][-1].value <= 1010): 
+            prediction += PRESSURE_ABS_SHARE
+        elif (data['pressure'][-1].value <= 1015):
+            prediction += PRESSURE_ABS_SHARE * pressure_delta
+        
+        if (pressure_change_delta >= 2):
+            prediction += PRESSURE_DELTA_SHARE
+        elif (pressure_change_delta >= 0):
+            prediction += PRESSURE_DELTA_SHARE * (pressure_change_delta / 2)
+
+        prediction += data['humidity'][-1].value / 100 * HUMIDITY_ABS_SHARE  
+        
+        if (humidity_delta > 0):
+            prediction += HUMIDITY_DELTA_SHARE * (humidity_delta / 100)
+
+        if wind_speed_mean > 15:
+            prediction += WIND_SPEED_SHARE
+        
+        if wind_speed_mean > 3:
+            prediction += WIND_DIRECTION_DELTA_SHARE * (1 - wind_direction_delta / 180)
+            if wind_direction_change_delta > 0:
+                prediction += WIND_DIRECTION_CHANGE_SHARE * (wind_direction_change_delta / 180)
+        
+        prediction += TEMPERATURE_ABS_SHARE * (1 - temperature_delta / 20)
+
+        if temperature_change_delta > 0:
+            prediction += TEMPERATURE_DELTA_SHARE * (temperature_change_delta / 20)
+
+
+    new_prediction = RainPrediction(prediction=prediction, created_at=datetime.now())
+    db.session.add(new_prediction)
+    db.session.commit()
+
+    
+    if prediction > 1:
+        return 1
+    elif prediction < 0:
+        return 0
+    
+    return prediction
